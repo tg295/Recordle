6e4421e598788b907c83cd42b11261b26f6d5706
Theo Gardner <theogardner@yahoo.co.uk>
2023-06-13 10:02:40 +0100

    finish add album script

--- backend/src/cli/add-albums-to-list.py
+++ backend/src/cli/add-albums-to-list.py
@@ -1,4 +1,7 @@
 import os
+import sys
+
+sys.path.append(os.path.join(os.getcwd()))
 
 import click
 
@@ -8,30 +11,27 @@ from aws_lambda_powertools import Logger
 
 root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# new_albums = [
-# 'https://open.spotify.com/album/0GPvraFrPjbXzY0H3fcudk?si=pcmN-0sTR3mlXs1ze27U2g',
-# 'https://open.spotify.com/album/6PanEvuo9ZNvGT39v50xp6?si=STpYdmlRQJ6LsISTwLx95g',
-# 'https://open.spotify.com/album/6haDxdERWK3rdVCGc2jr8E?si=QEWiTjfBSXyxOHW_DFoIeA'
-# ]
-
-
 logger = Logger(service=None, xray_trace_id=None, timestamp=None)
 
-@click.argument('album_id', type=str, help='Album ID, or comma separated list of IDs')
+@click.command()
+@click.argument('album_ids', type=str)
 def add_album(album_ids):
     """ Add album(s) to the list of albums to download """
     album_ids = album_ids.split(',')
-    albums = get_album_list()
+    albums = get_album_list(local=True)
+    logger.info("total albums in list: {}".format(len(albums)))
     
     albums.extend(album_ids)
     albums = list(set(albums))
-
+    
     prefix = root+'/data'
 
     with open('{}/albums_todo.txt'.format(prefix), 'w') as f:
         f.write('\n'.join(albums))
     upload_to_aws('{}/albums_todo.txt'.format(prefix), 'albums_todo.txt')
     logger.info('albums added: {}'.format(', '.join(album_ids)))
+    logger.info("total albums in list: {}".format(len(albums)))
+
 
 
 if __name__ == "__main__":
